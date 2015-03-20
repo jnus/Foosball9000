1d9a3af019969f992c357e4aeda5df17eb8113dd
Mads Skipper <madsskipper@gmail.com>
2015-03-20 23:41:43 +0100

    sdf

---
+++ RavenStarterKit/Controllers/LeaderboardViewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPWy.Controllers
+{
+    public class LeaderboardViewController : Controller
+    {
+        public List<string> GetAll()
+        {
+            return new List<string> {"sdf", "mads", "jlk"};
+        } 
+    }
+}
